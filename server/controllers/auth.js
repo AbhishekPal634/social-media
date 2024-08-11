46c1be232983e23fb2f05e484ec213ad1ba2c6e7
Abhishek Pal <abhishekpal1413@gmail.com>
2024-08-11 11:11:16 +0530

    server initialization and add user authentication

---
+++ server/controllers/auth.js
@@ -0,0 +1,70 @@
+import db from "../connect.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const register = async (req, res) => {
+  //Check user if exists
+  try {
+    const result = await db.query("SELECT * FROM users WHERE username = $1", [
+      req.body.username,
+    ]);
+    if (result.rows.length) return res.status(409).json("User already exists.");
+    //Create new user
+    //Hash password
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+
+    try {
+      await db.query(
+        "INSERT INTO users (username, email, password, name) VALUES ($1, $2, $3, $4)",
+        [req.body.username, req.body.email, hashedPassword, req.body.name]
+      );
+      return res.status(200).json("User has been created.");
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const result = await db.query("SELECT * FROM users WHERE username = $1", [
+      req.body.username,
+    ]);
+    if (result.rows.length === 0)
+      return res.status(404).json("User not found.");
+
+    const checkPassword = bcrypt.compareSync(
+      req.body.password,
+      result.rows[0].password
+    );
+
+    if (!checkPassword)
+      return res.status(400).json("Wrong username or password.");
+
+    const token = jwt.sign({ id: result.rows[0].id }, "secretkey"); //store secretkey in .env later
+
+    const { password, ...others } = result.rows[0];
+
+    res
+      .cookie("accessToken", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(others);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+
+export const logout = (req, res) => {
+  res
+    .clearCookie("accessToken", {
+      secure: true,
+      sameSite: "none",
+    })
+    .status(200)
+    .json("User has been logged out.");
+};
---
+++ server/index.js
@@ -0,0 +1,39 @@
+import Express from "express";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import likeRoutes from "./routes/likes.js";
+import commentRoutes from "./routes/comments.js";
+import db from "./connect.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = Express();
+const port = 8800;
+
+// Middlewares
+app.use(Express.json());
+app.use(cors());
+app.use(cookieParser());
+
+// Database connection
+db.connect((err) => {
+  if (err) {
+    console.error("Failed to connect to the database:", err.stack);
+    process.exit(1); // Exit if the database connection fails
+  } else {
+    console.log("Connected to the database.");
+
+    // Routes
+    app.use("/api/auth", authRoutes);
+    app.use("/api/users", userRoutes);
+    app.use("/api/posts", postRoutes);
+    app.use("/api/likes", likeRoutes);
+    app.use("/api/comments", commentRoutes);
+
+    // Start the server
+    app.listen(port, () => {
+      console.log(`Server running on http://localhost:${port}`);
+    });
+  }
+});
